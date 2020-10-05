29b4e72984e04d88754362dc02596a9799d2793e
Webber <webber@takken.io>
2020-10-05 21:19:37 +0200

    Deploy in europe

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,12 @@
-import * as functions from 'firebase-functions';
+import * as firebase from 'firebase-functions';
+
+// Set functions to same region as database and hosting
+const functions = firebase.region("europe-west3");
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+export const helloWorld = functions.https.onRequest((request, response) => {
+  firebase.logger.info("Hello logs!", {structuredData: true});
+  response.send("Hello from Unity CI Versioning Backend!");
+});
