ba69c187782ff9f418082c40dbec86c6bce0a8e7
Webber <webber@takken.io>
2020-10-06 23:20:57 +0200

    Organise functions in separate files

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,12 +1,3 @@
-import * as firebase from 'firebase-functions';
+const { helloWorld } = require('./helloWorld');
 
-// Set functions to same region as database and hosting
-const functions = firebase.region('europe-west3');
-
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-export const helloWorld = functions.https.onRequest((request, response) => {
-  firebase.logger.info('Hello logs!', { structuredData: true });
-  response.send('Hello from Unity CI Versioning Backend!');
-});
+exports.helloWorld = helloWorld;
