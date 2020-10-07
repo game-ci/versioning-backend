8e5f60dda4c33ea795b507f2049a71d15e2b2a09
Webber <webber@takken.io>
2020-10-07 19:06:05 +0200

    use JSDom instead

--- functions/src/ingest.ts
+++ functions/src/ingest.ts
@@ -1,40 +1,32 @@
 import { Request } from 'firebase-functions/lib/providers/https';
 
 const { firebase, functions } = require('./shared');
-const Browser = require('./services/browser');
+const { getDocumentFromUrl } = require('./utils/get-document-from-url');
 
 /**
  * Based on https://github.com/BLaZeKiLL/unity-scraper
  */
-export const ingest = functions
-  .runWith({ memory: '512MB', timeoutSeconds: 90 })
-  .https.onRequest(async (request: Request, response: any) => {
-    const browser = await Browser.getInstance();
+export const ingest = functions.https.onRequest(async (request: Request, response: any) => {
+  try {
+    const archiveUrl = 'https://unity3d.com/get-unity/download/archive';
 
-    try {
-      const archiveUrl = 'https://unity3d.com/get-unity/download/archive';
-
-      const page = await browser.newPage();
-      await page.goto(archiveUrl);
-
-      const hrefs = await page.evaluate(() =>
-        Array.from(document.querySelectorAll('a.unityhub[href]'), (a) => a.getAttribute('href')),
-      );
-
-      const versionInfo = hrefs.map((href: string) => {
-        const link = href.replace('unityhub://', '');
+    const document = await getDocumentFromUrl(archiveUrl);
+    const versionInfo = Array.from(
+      // eslint-disable-next-line @typescript-eslint/no-unnecessary-type-assertion
+      document.querySelectorAll('.unityhub') as NodeListOf<HTMLAnchorElement>,
+      (a) => {
+        const link = a.href.replace('unityhub://', '');
 
         return {
           version: link.split('/')[0],
           changeset: link.split('/')[1],
         };
-      });
+      },
+    );
 
-      response.send(versionInfo);
-    } catch (err) {
-      firebase.logger.error(err);
-      response.send('Oops.');
-    } finally {
-      await browser.close();
-    }
-  });
+    response.send(versionInfo);
+  } catch (err) {
+    firebase.logger.error(err);
+    response.send('Oops.');
+  }
+});
