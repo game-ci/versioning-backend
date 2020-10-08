68608efee1d79bae9aa733c83b4013fd7c89e8c9
Webber <webber@takken.io>
2020-10-08 20:22:09 +0200

    disable buggy import/no-unresolved in eslint

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -24,6 +24,7 @@ module.exports = {
     'comma-dangle': ['warn', 'always-multiline'],
     'constructor-super': 'error',
     eqeqeq: ['warn', 'always'],
+    'import/no-unresolved': 0, // TODO - sync with tsconfig somehow
     'import/no-deprecated': 'warn',
     'import/no-extraneous-dependencies': 'error',
     'import/no-unassigned-import': 'warn',
