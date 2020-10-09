e824cf592f9ea2ec3b1a0a51a315139b20607ec6
Webber <webber@takken.io>
2020-10-09 13:47:04 +0200

    Display ingested versions

--- functions/src/api/unityVersions.ts
+++ functions/src/api/unityVersions.ts
@@ -1,13 +1,17 @@
 import { Request } from 'firebase-functions/lib/providers/https';
 import { Response } from 'express-serve-static-core';
-import { firebase, functions } from '../shared';
-import { getUnityVersionInfo } from '../logic/getUnityVersionInfo';
+import { db, firebase, functions } from '../config/firebase';
+import { VersionInfo } from '../model/versionInfo';
 
 export const unityVersions = functions.https.onRequest(
   async (request: Request, response: Response) => {
     try {
-      const versionInfo = await getUnityVersionInfo();
-      response.send(versionInfo);
+      const snapshot = await db.collection('unityVersions').orderBy('version', 'desc').get();
+      const versionsInfoList = snapshot.docs.map((doc) => doc.data()) as VersionInfo[];
+
+      const versions = versionsInfoList.map((versionInfo) => versionInfo.version);
+
+      response.send(versions);
     } catch (err) {
       firebase.logger.error(err);
       response.send('Oops.');
