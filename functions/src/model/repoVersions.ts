d8f09d69f549c78ba06f5b1ee960110a0dbece6a
Webber <webber@takken.io>
2020-10-11 21:21:33 +0200

    Add model for repository versions

---
+++ functions/src/model/repoVersions.ts
@@ -0,0 +1,56 @@
+import { db, admin } from '../config/firebase';
+import Timestamp = admin.firestore.Timestamp;
+
+const COLLECTION = 'repoVersions';
+
+export interface RepoVersionInfo {
+  version: string;
+  major: number;
+  minor: number;
+  patch: string;
+  addedDate?: Timestamp;
+  modifiedDate?: Timestamp;
+}
+
+export class RepoVersionInfo {
+  static getLatest = async (): Promise<RepoVersionInfo> => {
+    const snapshot = await db
+      .collection(COLLECTION)
+      .orderBy('major', 'desc')
+      .orderBy('minor', 'desc')
+      .orderBy('patch', 'desc')
+      .limit(1)
+      .get();
+
+    return snapshot.docs[0].data() as RepoVersionInfo;
+  };
+
+  static create = async (repoVersion: RepoVersionInfo) => {
+    const { version } = repoVersion;
+    await db
+      .collection(COLLECTION)
+      .doc(version)
+      .set(
+        {
+          ...repoVersion,
+          addedDate: Timestamp.now(),
+          modifiedDate: Timestamp.now(),
+        },
+        { merge: false },
+      );
+  };
+
+  static update = async (repoVersion: RepoVersionInfo) => {
+    const { version } = repoVersion;
+    await db
+      .collection(COLLECTION)
+      .doc(version)
+      .set(
+        {
+          ...repoVersion,
+          modifiedDate: Timestamp.now(),
+        },
+        { merge: true },
+      );
+  };
+}
