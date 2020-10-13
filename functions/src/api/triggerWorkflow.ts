bf3763413a6ab36c1761424893e6bcd4b20f5414
Webber <webber@takken.io>
2020-10-14 00:39:12 +0200

    Workflow now works

--- functions/src/api/triggerWorkflow.ts
+++ functions/src/api/triggerWorkflow.ts
@@ -6,19 +6,50 @@ import { GitHub } from '../config/github';
 export const triggerWorkflow = functions.https.onRequest(
   async (request: Request, response: Response) => {
     try {
+      // @ts-ignore
       const gitHub = await GitHub.init();
 
       // This works only in the "installation" auth scope.
-      const result = await gitHub.repos.createDispatchEvent({
-        owner: 'unity-ci',
-        repo: 'docker',
-        event_type: 'new_build_requested',
-        client_payload: {
-          test_var: 'test value',
-        },
-      });
 
-      response.status(200).send(result);
+      // // Base - SUCCESS
+      // const result = await gitHub.repos.createDispatchEvent({
+      //   owner: 'unity-ci',
+      //   repo: 'docker',
+      //   event_type: 'new_base_image_requested',
+      //   client_payload: {
+      //     patchTag: '0.1.0',
+      //     minorTag: '0.1',
+      //     majorTag: '0',
+      //   },
+      // });
+
+      // // Hub - SUCCESS
+      // const result = await gitHub.repos.createDispatchEvent({
+      //   owner: 'unity-ci',
+      //   repo: 'docker',
+      //   event_type: 'new_hub_image_requested',
+      //   client_payload: {
+      //     patchTag: '0.1.0',
+      //     minorTag: '0.1',
+      //     majorTag: '0',
+      //   },
+      // });
+
+      // // Editor - SUCCESS
+      // const result = await gitHub.repos.createDispatchEvent({
+      //   owner: 'unity-ci',
+      //   repo: 'docker',
+      //   event_type: 'new_editor_image_requested',
+      //   client_payload: {
+      //     unityVersion: '2019.3.11f1',
+      //     changeSet: 'ceef2d848e70',
+      //     patchTag: '0.1.0',
+      //     minorTag: '0.1',
+      //     majorTag: '0',
+      //   },
+      // });
+
+      response.status(200).send(':)');
     } catch (err) {
       firebase.logger.error(err);
       response.status(500).send('Oops.');
