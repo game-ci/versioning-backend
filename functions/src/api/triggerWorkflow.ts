74f2521a29b5f314d2f4f2b111c925f624cd077d
Webber Takken <webber@takken.io>
2020-10-16 17:46:43 +0200

    Sync interfaces for two-way communication to docker repo (#13)
    * Workflow now works
    
    * Update interface to match incoming info

--- functions/src/api/triggerWorkflow.ts
+++ functions/src/api/triggerWorkflow.ts
@@ -6,19 +6,56 @@ import { GitHub } from '../config/github';
 export const triggerWorkflow = functions.https.onRequest(
   async (request: Request, response: Response) => {
     try {
+      // @ts-ignore
       const gitHub = await GitHub.init();
 
       // This works only in the "installation" auth scope.
-      const result = await gitHub.repos.createDispatchEvent({
-        owner: 'unity-ci',
-        repo: 'docker',
-        event_type: 'new_build_requested',
-        client_payload: {
-          test_var: 'test value',
-        },
-      });
 
-      response.status(200).send(result);
+      // // Base - SUCCESS
+      // const result = await gitHub.repos.createDispatchEvent({
+      //   owner: 'unity-ci',
+      //   repo: 'docker',
+      //   event_type: 'new_base_image_requested',
+      //   client_payload: {
+      //     jobId: 'someJobId',
+      //     buildId: 'someBuildId',
+      //     patchTag: '0.2.0',
+      //     minorTag: '0.2',
+      //     majorTag: '0',
+      //   },
+      // });
+
+      // // Hub - SUCCESS
+      // const result = await gitHub.repos.createDispatchEvent({
+      //   owner: 'unity-ci',
+      //   repo: 'docker',
+      //   event_type: 'new_hub_image_requested',
+      //   client_payload: {
+      //     jobId: 'someJobId',
+      //     buildId: 'someBuildId',
+      //     patchTag: '0.2.0',
+      //     minorTag: '0.2',
+      //     majorTag: '0',
+      //   },
+      // });
+
+      // // Editor - SUCCESS
+      // const result = await gitHub.repos.createDispatchEvent({
+      //   owner: 'unity-ci',
+      //   repo: 'docker',
+      //   event_type: 'new_editor_image_requested',
+      //   client_payload: {
+      //     jobId: 'someJobId',
+      //     buildId: 'someBuildId',
+      //     unityVersion: '2019.3.11f1',
+      //     changeSet: 'ceef2d848e70',
+      //     patchTag: '0.2.0',
+      //     minorTag: '0.2',
+      //     majorTag: '0',
+      //   },
+      // });
+
+      response.status(200).send(':)');
     } catch (err) {
       firebase.logger.error(err);
       response.status(500).send('Oops.');
