98b166ecabe3b9254716623a24eed5e05cd5cc90
Webber <webber@takken.io>
2020-10-16 20:47:19 +0200

    yea that didn't work

--- functions/src/cron/index.ts
+++ functions/src/cron/index.ts
@@ -1,2 +1,30 @@
-export { trigger } from './trigger';
-export { worker } from './worker';
+import { EventContext } from 'firebase-functions';
+import { firebase, functions } from '../config/firebase';
+import { Discord } from '../config/discord';
+import { ingestUnityVersions } from '../logic/ingestUnityVersions';
+
+/**
+ * CPU-time for pubSub is not part of the free quota, so we'll keep it light weight.
+ * This will call the cloud function `cron/worker`, using an authentication token.
+ */
+export const trigger = functions.pubsub
+  .schedule('every 15 minutes')
+  .onRun(async (context: EventContext) => {
+    try {
+      await routineTasks();
+    } catch (err) {
+      const message = `
+        Something went wrong while wrong while running routine tasks.
+        ${err.message} (${err.status})\n${err.stackTrace}
+      `;
+
+      firebase.logger.error(message);
+      await Discord.sendAlert(message);
+    } finally {
+      await Discord.disconnect();
+    }
+  });
+
+const routineTasks = async () => {
+  await ingestUnityVersions();
+};
