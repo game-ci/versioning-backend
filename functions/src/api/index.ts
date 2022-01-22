f334c1f3d282e7fc4b77811ef66489320e29f802
Webber <webber@takken.io>
2022-01-22 23:30:13 +0100

    chore: remove manual workflow triggers in favour of github manual dispatch in docker repo

--- functions/src/api/index.ts
+++ functions/src/api/index.ts
@@ -3,5 +3,4 @@ export { reportBuildFailure } from './reportBuildFailure';
 export { reportNewBuild } from './reportNewBuild';
 export { reportPublication } from './reportPublication';
 export { unityVersions } from './unityVersions';
-export { manualWorkflowTrigger } from './manualWorkflowTrigger';
 export { retryBuild } from './retryBuild';
