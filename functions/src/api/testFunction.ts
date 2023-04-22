d12b2accb487c4e24e7e83afbd097575686a44be
Andrew Kahr <22359829+AndrewKahr@users.noreply.github.com>
2023-04-22 14:26:55 -0700

    Add test function deployment

---
+++ functions/src/api/testFunction.ts
@@ -0,0 +1,9 @@
+import { Request } from 'firebase-functions/v2/https';
+import { Response } from 'express-serve-static-core';
+import { functions } from '../service/firebase';
+
+export const testFunction = functions.https.onRequest(
+  async (request: Request, response: Response) => {
+
+  },
+);
