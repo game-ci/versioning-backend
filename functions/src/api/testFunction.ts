db209472eec7b24484c9a5dea1505790b135a4c0
Andrew Kahr <22359829+AndrewKahr@users.noreply.github.com>
2023-04-22 14:32:03 -0700

    Fix linting

--- functions/src/api/testFunction.ts
+++ functions/src/api/testFunction.ts
@@ -3,5 +3,6 @@ import { Response } from 'express-serve-static-core';
 import { functions } from '../service/firebase';
 
 export const testFunction = functions.https.onRequest(
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
   async (request: Request, response: Response) => {},
 );
