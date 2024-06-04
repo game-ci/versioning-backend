2280e5ea58b0270eaafb03b468a3f8f8b4176a5c
Andrew Kahr <22359829+AndrewKahr@users.noreply.github.com>
2024-06-04 02:22:40 -0700

    Verbose secret paths

--- functions/src/api/testFunction.ts
+++ functions/src/api/testFunction.ts
@@ -2,10 +2,14 @@ import { onRequest, Request } from 'firebase-functions/v2/https';
 import { Response } from 'express-serve-static-core';
 import { defineSecret } from 'firebase-functions/params';
 
-const discordToken = defineSecret('DISCORD_TOKEN');
-const githubPrivateKeyConfigSecret = defineSecret('GITHUB_PRIVATE_KEY');
-const githubClientSecretConfigSecret = defineSecret('GITHUB_CLIENT_SECRET');
-const internalToken = defineSecret('INTERNAL_TOKEN');
+const discordToken = defineSecret('projects/841479186623/secrets/DISCORD_TOKEN');
+const githubPrivateKeyConfigSecret = defineSecret(
+  'projects/841479186623/secrets/GITHUB_PRIVATE_KEY',
+);
+const githubClientSecretConfigSecret = defineSecret(
+  'projects/841479186623/secrets/GITHUB_CLIENT_SECRET',
+);
+const internalToken = defineSecret('projects/841479186623/secrets/INTERNAL_TOKEN');
 
 export const testFunction = onRequest(
   {
--- functions/src/config/credential.ts
+++ functions/src/config/credential.ts
@@ -1,9 +1,8 @@
 import { credential } from 'firebase-admin';
-import { readFileSync } from 'node:fs';
 
 export const getCredential = (): credential.Credential => {
   try {
-    const serviceAccount = readFileSync('../../service-account.json').toString();
+    const serviceAccount = require('../../service-account.json');
     return credential.cert(serviceAccount);
   } catch (e) {
     return credential.applicationDefault();
