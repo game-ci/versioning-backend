f605d874ab25dd30365118aad4d53f42af2516c8
Andrew Kahr <22359829+AndrewKahr@users.noreply.github.com>
2024-06-04 02:34:07 -0700

    Force service account credential

--- functions/src/config/credential.ts
+++ functions/src/config/credential.ts
@@ -2,8 +2,7 @@ import { credential } from 'firebase-admin';
 
 export const getCredential = (): credential.Credential => {
   try {
-    const serviceAccount = require(process.env.GOOGLE_APPLICATION_CREDENTIALS ??
-      '../../service-account.json');
+    const serviceAccount = require(process.env.GOOGLE_APPLICATION_CREDENTIALS ?? '');
     return credential.cert(serviceAccount);
   } catch (e) {
     return credential.applicationDefault();
