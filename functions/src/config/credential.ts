42782bd7da8f9610f3f3c449824fa54022ee420d
Andrew Kahr <22359829+AndrewKahr@users.noreply.github.com>
2024-06-04 02:51:42 -0700

    Add fallback path to service account credentials

--- functions/src/config/credential.ts
+++ functions/src/config/credential.ts
@@ -2,7 +2,8 @@ import { credential } from 'firebase-admin';
 
 export const getCredential = (): credential.Credential => {
   try {
-    const serviceAccount = require(process.env.GOOGLE_APPLICATION_CREDENTIALS ?? '');
+    const serviceAccount = require(process.env.GOOGLE_APPLICATION_CREDENTIALS ??
+      '../../service-account.json');
     return credential.cert(serviceAccount);
   } catch (e) {
     return credential.applicationDefault();
