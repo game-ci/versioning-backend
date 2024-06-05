0828f6a67db61ac2ce15052386e40c3a42905e9a
Andrew Kahr <22359829+AndrewKahr@users.noreply.github.com>
2024-06-04 22:19:05 -0700

    Improved deployment tests. More github auth fixes

--- functions/src/api/testFunction.ts
+++ functions/src/api/testFunction.ts
@@ -1,6 +1,7 @@
 import { onRequest, Request } from 'firebase-functions/v2/https';
 import { Response } from 'express-serve-static-core';
 import { defineSecret } from 'firebase-functions/params';
+import { scrapeVersions } from '../logic/ingestRepoVersions/scrapeVersions';
 
 const discordToken = defineSecret('DISCORD_TOKEN');
 const githubPrivateKeyConfigSecret = defineSecret('GITHUB_PRIVATE_KEY');
@@ -9,7 +10,7 @@ const internalToken = defineSecret('INTERNAL_TOKEN');
 
 export const testFunction = onRequest(
   {
-    // Passing secrets so that test deployments verify that the secrets are correctly set.
+    // Passing all secrets so that test deployments verify that the secrets are correctly set.
     secrets: [
       discordToken,
       githubPrivateKeyConfigSecret,
@@ -17,6 +18,18 @@ export const testFunction = onRequest(
       internalToken,
     ],
   },
-  // eslint-disable-next-line @typescript-eslint/no-empty-function
-  async (request: Request, response: Response) => {},
+  async (request: Request, response: Response) => {
+    // Run all non-sensitive functions to verify that the deployment is working.
+    const versions = await scrapeVersions(
+      githubPrivateKeyConfigSecret.value(),
+      githubClientSecretConfigSecret.value(),
+    );
+
+    if (versions.length === 0) {
+      response.status(500).send('No versions were found.');
+      return;
+    }
+
+    response.status(200).send('Ok');
+  },
 );
