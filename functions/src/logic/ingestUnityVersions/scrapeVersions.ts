ffdde860c9172c34445655e67b947e71516b9e5b
Louis Delassus <ld@immersiv.io>
2024-10-12 00:49:11 +0200

    Fix unity versions scrapping not getting all the versions

--- functions/src/logic/ingestUnityVersions/scrapeVersions.ts
+++ functions/src/logic/ingestUnityVersions/scrapeVersions.ts
@@ -9,12 +9,13 @@ export const scrapeVersions = async (): Promise<EditorVersionInfo[]> => {
 
   const scripts = document.querySelectorAll('script');
 
+  const allVersions = new Map<string, EditorVersionInfo>();
+
   for (const script of scripts) {
     if (script.textContent) {
       const matches = [...script.textContent.matchAll(unity_version_regex)];
       if (matches.length > 0) {
-        const uniqueVersions = new Set<string>();
-        return matches
+        const versions = matches
           .filter((match) => {
             // Filter out prerelease and unsupported versions
             const [_, major, minor, patch, changeSet] = match;
@@ -23,8 +24,7 @@ export const scrapeVersions = async (): Promise<EditorVersionInfo[]> => {
           .map((match) => {
             const [_, major, minor, patch, changeSet] = match;
             const version = `${major}.${minor}.${patch}`;
-            if (!uniqueVersions.has(version)) {
-              uniqueVersions.add(version);
+            if (!allVersions.has(version)) {
               return {
                 version,
                 changeSet,
@@ -38,8 +38,16 @@ export const scrapeVersions = async (): Promise<EditorVersionInfo[]> => {
             return null;
           })
           .filter((version) => version !== null) as EditorVersionInfo[];
+
+        versions.forEach((it) => {
+          allVersions.set(it.version, it);
+        });
       }
     }
+  }
+
+  if (allVersions.size > 0) {
+    return Array.from(allVersions.values());
   }
 
   throw new Error('No Unity versions found!');
